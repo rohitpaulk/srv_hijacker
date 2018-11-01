582aaaf5e4e671f48b1ae0b2344fa5a26b88fb53
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-11-02 00:09:56 +0530

    Simplify patching API

--- srv_hijacker/srv_hijacker.py
+++ srv_hijacker/srv_hijacker.py
@@ -10,18 +10,16 @@ from urllib3.util import connection
 from dns import resolver
 
 
-def resolve_srv_record(host, dns_ip, dns_port):
-    res = resolver.Resolver()
-
-    res.port = int(dns_port)
-    res.nameservers = [dns_ip]
-
-    ans = res.query(host, 'SRV')
+def resolve_srv_record(host, resolver):
+    ans = resolver.query(host, 'SRV')
 
     return ans.response.additional[0].items[0].address, ans[0].port
 
 
-def patched_new_conn(url_regex, srv_dns_host, srv_dns_port):
+original_new_conn = HTTPConnection._new_conn
+
+
+def patched_new_conn(url_regex, srv_resolver):
     """
     Returns a function that does pretty much what
     `urllib3.connection.HTTPConnection._new_conn` does.
@@ -29,50 +27,22 @@ def patched_new_conn(url_regex, srv_dns_host, srv_dns_port):
     url_regex:
 
     The regex to match a host against. If this regex matches the host, we
-    hit the srv_dns_host to fetch the new host + port
-
-    srv_dns_host, srv_dns_port:
-
-    host and port for the dns server. Example: "127.0.0.1", "8600"
+    hit the srv_resolver to fetch the new host + port
     """
 
-    def new_host_and_port(old_host, old_port):
-        if re.search(url_regex, old_host):
-            host, port = resolve_srv_record(old_host, srv_dns_host,
-                                            srv_dns_port)
-        else:
-            host = old_host
-            port = old_port
+    def patched_f(self):
+        if re.search(url_regex, self.host):
+            self.host, self.port = resolve_srv_record(self.host, srv_resolver)
 
-        return host, port
+        return original_new_conn(self)
 
-    def f(self):
-        hostname, port = new_host_and_port(self.host, self.port)
-
-        extra_kw = {}
-        if self.source_address:
-            extra_kw['source_address'] = self.source_address
-        if self.socket_options:
-            extra_kw['socket_options'] = self.socket_options
-
-        try:
-            conn = connection.create_connection((hostname, port), self.timeout,
-                                                **extra_kw)
-
-        except SocketTimeout as e:
-            raise ConnectTimeoutError(
-                self, "Connection to %s timed out. (connect timeout=%s)" %
-                (self.host, self.timeout))
-
-        except SocketError as e:
-            raise NewConnectionError(
-                self, "Failed to establish a new connection: %s" % e)
-
-        return conn
-
-    return f
+    return patched_f
 
 
 def hijack(host_regex, srv_dns_host, srv_dns_port):
-    HTTPConnection._new_conn = patched_new_conn(host_regex, srv_dns_host,
-                                                srv_dns_port)
+    srv_resolver = resolver.Resolver()
+
+    srv_resolver.port = int(srv_dns_port)
+    srv_resolver.nameservers = [srv_dns_host]
+
+    HTTPConnection._new_conn = patched_new_conn(host_regex, srv_resolver)
