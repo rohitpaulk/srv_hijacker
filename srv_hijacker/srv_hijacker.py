36961347553a411e6d0a14c1c767aa4cd0a40b18
Paul Kuruvilla <rohitpaulk@gmail.com>
2018-11-13 15:29:06 +0530

    Add debug logs

--- srv_hijacker/srv_hijacker.py
+++ srv_hijacker/srv_hijacker.py
@@ -9,6 +9,10 @@ from urllib3.util import connection
 
 from dns import resolver
 
+import logging
+
+logger = logging.getLogger("srv_hijacker")
+
 
 def resolve_ip_for_target(rrsets, target):
     for rrset in rrsets:
@@ -18,13 +22,16 @@ def resolve_ip_for_target(rrsets, target):
     raise NewConnectionError("Couldn't find A record for target %s" % target)
 
 
-def resolve_srv_record(host, resolver):
-    ans = resolver.query(host, 'SRV')
+def resolve_srv_record(old_host, resolver):
+    ans = resolver.query(old_host, 'SRV')
 
-    port = ans[0].port
-    host = resolve_ip_for_target(ans.response.additional, ans[0].target)
+    new_port = ans[0].port
+    new_host = resolve_ip_for_target(ans.response.additional, ans[0].target)
 
-    return host, port
+    logger.debug("Resolved SRV record for host %s: (%s:%s)", old_host,
+                 new_host, new_port)
+
+    return new_host, new_port
 
 
 original_new_conn = HTTPConnection._new_conn
@@ -43,7 +50,11 @@ def patched_new_conn(url_regex, srv_resolver):
 
     def patched_f(self):
         if re.search(url_regex, self.host):
+            logger.debug("Host %s matched SRV regex, resolving", self.host)
             self.host, self.port = resolve_srv_record(self.host, srv_resolver)
+        else:
+            logger.debug("Host %s did not match SRV regex, ignoring",
+                         self.host)
 
         return original_new_conn(self)
 
