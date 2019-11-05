a94a688ddf074a8703e28b8c6a4066240cb3d440
Paul Kuruvilla <rohitpaulk@gmail.com>
2019-11-05 15:44:41 +0530

    Patch socket directly, not just HTTP

--- tests/test_srv_hijacker.py
+++ tests/test_srv_hijacker.py
@@ -1,6 +1,7 @@
-import srv_hijacker
-import requests
 import pytest
+import requests
+
+import srv_hijacker
 
 CONSUL_HOST = "127.0.0.1"
 CONSUL_DNS_PORT = "8600"
@@ -8,10 +9,23 @@ CONSUL_API_PORT = "8500"
 CONSUL_API_URL = f"{CONSUL_HOST}:{CONSUL_API_PORT}"
 
 
-def test_hijack(consul_test_service_url):
-    # Before 'hijack' is run, this request must fail
+def test_hijack(consul_http_service_url):
+    def test_http(url):
+        response = requests.get(url)
+        assert response.status_code == 200
+
+    def test_http_direct():
+        test_http("https://httpbin.org/get")
+
+    def test_http_hijacked():
+        test_http(consul_http_service_url)
+
+    # Before 'hijack' is run, direct requests must pass
+    test_http_direct()
+
+    # Before 'hijack' is run, hijacked requests must fail
     with pytest.raises(requests.exceptions.ConnectionError):
-        requests.get(consul_test_service_url)
+        test_http_hijacked()
 
     srv_hijacker.hijack(
         host_regex=r"service.consul$",
@@ -19,13 +33,9 @@ def test_hijack(consul_test_service_url):
         srv_dns_port=CONSUL_DNS_PORT,
     )
 
-    # Now that the monkey patching is done, this should succeed
-    response = requests.get(consul_test_service_url)
-    assert response.status_code == 200
-
-    # Making sure a normal requests passes even after the patch is done
-    response = requests.get("https://httpbin.org/get")
-    assert response.status_code == 200
+    # Now that the monkey patching is done, both should succeed
+    test_http_direct()
+    test_http_hijacked()
 
     # Patching again shouldn't cause issues
     srv_hijacker.hijack(
@@ -34,19 +44,17 @@ def test_hijack(consul_test_service_url):
         srv_dns_port=CONSUL_DNS_PORT,
     )
 
-    response = requests.get(consul_test_service_url)
-    assert response.status_code == 200
-    response = requests.get("https://httpbin.org/get")
-    assert response.status_code == 200
+    test_http_direct()
+    test_http_hijacked()
 
 
 @pytest.fixture
-def consul_test_service_url():
+def consul_http_service_url():
     # Let's register a service named 'test'  to point back to consul itself
-    register_service_on_consul("test", CONSUL_HOST, CONSUL_API_PORT)
+    register_service_on_consul("test-http", CONSUL_HOST, CONSUL_API_PORT)
 
     # We can now use a consul endpoint itself for testing.
-    return "http://test.service.consul/v1/agent/services"
+    return "http://test-http.service.consul/v1/agent/services"
 
 
 def register_service_on_consul(service_name, service_host, service_port):
